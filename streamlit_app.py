6db3e1fde4822020ad24c04f25612a33db20a91a
Kyi Thar <khukt@users.noreply.github.com>
2024-06-12 00:14:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -218,19 +218,17 @@ def normalize_attention_weights(attentions):
     # Assuming we want the average of attention weights across all heads
     avg_attentions = torch.mean(last_layer_attentions, dim=1)
     # Normalize the attention weights to be between 0 and 1
-    normalized_attentions = avg_attentions[0].cpu().detach().numpy()
-    return normalized_attentions / normalized_attentions.max()
+    normalized_attentions = avg_attentions / avg_attentions.max()
+    return normalized_attentions[0].cpu().detach().numpy()
 
-def highlight_text(tokenizer, input_ids, attention_weights):
+def highlight_text(tokenizer, input_text, input_ids, attention_weights):
     tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
-    # Flatten attention weights if necessary and ensure correct length
-    if attention_weights.ndim > 1:
-        attention_weights = attention_weights.flatten()
     assert len(tokens) == len(attention_weights), "Mismatch between tokens and attention weights length"
     highlighted_text = ""
     for token, weight in zip(tokens, attention_weights):
-        token = token.replace('▁', '')  # Remove special character for readability
-        color = f"rgba(255, 0, 0, {weight:.2f})"  # Red color with transparency based on attention weight
+        if token.startswith('▁'):
+            token = token[1:]
+        color = f"rgba(255, 0, 0, {weight})"  # Red color with transparency based on attention weight
         highlighted_text += f"<span style='background-color: {color}'>{token}</span> "
     return highlighted_text
 
