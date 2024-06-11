90f7c048161f678f4614e79d7262a071afb98ae8
Kyi Thar <khukt@users.noreply.github.com>
2024-06-11 23:10:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -331,6 +331,7 @@ if question:
     # Normalize and highlight the input text
     if attentions is not None:
         normalized_attentions = normalize_attention_weights(attentions)
+        st.write(normalized_attentions)
         highlighted_text = highlight_text(tokenizer, input_text, normalized_attentions)
         st.subheader("Highlighted Input Text Based on Attention Weights")
         st.markdown(f"<div style='border: 1px solid #ccc; padding: 10px; border-radius: 5px;'>{highlighted_text}</div>", unsafe_allow_html=True)
